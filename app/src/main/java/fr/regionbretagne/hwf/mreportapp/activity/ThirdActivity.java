c051be47fef50726bf25924e06cbb611b36f732c
HWFord <lilypickle@hotmail.fr>
2021-04-27 13:43:13 +0200

    third activity -- add xml and link class

--- app/src/main/java/fr/regionbretagne/hwf/mreportapp/activity/ThirdActivity.java
+++ app/src/main/java/fr/regionbretagne/hwf/mreportapp/activity/ThirdActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import fr.regionbretagne.hwf.mreportapp.R;
+
 public class ThirdActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
     }
 }
