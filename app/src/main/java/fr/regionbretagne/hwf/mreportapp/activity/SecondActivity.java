0e1bcfcf6898ee9e2d1b3d20fb329d93fd0c3be0
HWFord <lilypickle@hotmail.fr>
2021-04-27 16:20:26 +0200

    second activity -- get json and log content uses utils to get json from assets

--- app/src/main/java/fr/regionbretagne/hwf/mreportapp/activity/SecondActivity.java
+++ app/src/main/java/fr/regionbretagne/hwf/mreportapp/activity/SecondActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,18 +14,28 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 import fr.regionbretagne.hwf.mreportapp.R;
+import fr.regionbretagne.hwf.mreportapp.Utils.Utils;
 import fr.regionbretagne.hwf.mreportapp.adapter.RapportListAdapter;
 import fr.regionbretagne.hwf.mreportapp.model.Rapport;
 
 public class SecondActivity extends Activity {
 
+    private static final String TAG = "RapportListTag";
+
     private ListView lvSecondReportList;
     private Button btnSecondConnexion;
     private Button btnSecondRapports;
     private TextView tvSecondUrl;
+    private TextView tvSecondjson;
 
     private String url ="";
 
@@ -42,11 +53,26 @@ public class SecondActivity extends Activity {
         btnSecondConnexion = findViewById(R.id.btnSecondConnexion);
         btnSecondRapports = findViewById(R.id.btnSecondRapports);
         tvSecondUrl = findViewById(R.id.tvSecondUrl);
+        tvSecondjson = findViewById(R.id.tvSecondjson);
+
+        String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "jsonrapport.json");
+        Log.d("RapportListTag", jsonFileString);
+
+        Gson gson = new Gson();
+        Type listRapportType = new TypeToken<List<Rapport>>() { }.getType();
+
+        List<Rapport> rapports = gson.fromJson(jsonFileString, listRapportType);
+        for(int i =0; i< rapports.size(); i++){
+            Log.d("RapportListTag", "> Item " + i + "\n" + rapports.get(i));
+        }
+
+
+
 
 
         adapter = new RapportListAdapter(this, rapportsArray);
         lvSecondReportList.setAdapter(adapter);
-//
+
         SharedPreferences preferences = getSharedPreferences("url",0);
         url = preferences.getString("url", "");
 
@@ -70,4 +96,20 @@ public class SecondActivity extends Activity {
     public void openThirdActivity(){
         startActivity(intent);
     }
+
+//    public String loadJSONFromAsset() {
+//        String json = null;
+//        try {
+//            InputStream is = getActivity().getAssets().open("yourfilename.json");
+//            int size = is.available();
+//            byte[] buffer = new byte[size];
+//            is.read(buffer);
+//            is.close();
+//            json = new String(buffer, "UTF-8");
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//            return null;
+//        }
+//        return json;
+//    }
 }
