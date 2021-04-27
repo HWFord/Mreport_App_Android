80899d51f30a40f91c74373a277e75298660f50d
HWFord <lilypickle@hotmail.fr>
2021-04-27 11:54:17 +0200

    third Activity -- init

---
+++ app/src/main/java/fr/regionbretagne/hwf/mreportapp/activity/ThirdActivity.java
@@ -0,0 +1,13 @@
+package fr.regionbretagne.hwf.mreportapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ThirdActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
