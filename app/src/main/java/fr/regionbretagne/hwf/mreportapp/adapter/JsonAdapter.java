c66757dcfc94c92722d604ee911d048d139d0da3
HWFord <lilypickle@hotmail.fr>
2021-04-28 17:21:12 +0200

    listview adapter -- replace rapport Adpater with json adapter

---
+++ app/src/main/java/fr/regionbretagne/hwf/mreportapp/adapter/JsonAdapter.java
@@ -0,0 +1,52 @@
+package fr.regionbretagne.hwf.mreportapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import fr.regionbretagne.hwf.mreportapp.R;
+import fr.regionbretagne.hwf.mreportapp.model.Rapport;
+
+public class JsonAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<Rapport> arrayList;
+
+    public JsonAdapter(Context context, ArrayList<Rapport> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView ==  null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_rapport, parent, false);
+        }
+        TextView tvRapportTitle;
+        tvRapportTitle = (TextView) convertView.findViewById(R.id.tvRapportTitle);
+
+        tvRapportTitle.setText(arrayList.get(position).getTitle());
+
+        return convertView;
+    }
+}
