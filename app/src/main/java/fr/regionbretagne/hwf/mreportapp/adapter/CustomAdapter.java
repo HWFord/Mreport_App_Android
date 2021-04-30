3fab33cde71b3b5daf67ae21e7799817767e86d4
HWFord <lilypickle@hotmail.fr>
2021-04-30 17:20:31 +0200

    CustomAdapter -- update to open report

--- app/src/main/java/fr/regionbretagne/hwf/mreportapp/adapter/CustomAdapter.java
+++ app/src/main/java/fr/regionbretagne/hwf/mreportapp/adapter/CustomAdapter.java
@@ -1,10 +1,16 @@
 package fr.regionbretagne.hwf.mreportapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -39,13 +45,38 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+//        Rapport rapport = getItem(position);
         if (convertView ==  null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.item_rapport, parent, false);
         }
         TextView tvRapportTitle;
+        LinearLayout llSecondRapportBlock;
+        Button btnRapportAfficher;
+
         tvRapportTitle = (TextView) convertView.findViewById(R.id.tvRapportTitle);
+        llSecondRapportBlock = (LinearLayout) convertView.findViewById(R.id.llSecondRapportBlock);
+        btnRapportAfficher = (Button) convertView.findViewById(R.id.btnRapportAfficher);
 
         tvRapportTitle.setText(arrayList.get(position).getTitle());
+
+
+        SharedPreferences preferences = context.getSharedPreferences("url",0);
+        String url = preferences.getString("url", "");
+        String reportId = arrayList.get(position).getId();
+
+        btnRapportAfficher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        //add condition for slash
+                        Uri.parse(url + "mreport/" + reportId));
+                context.startActivity(browserIntent);
+            }
+        });
+
+//        if(position%2 ==0){
+//            llSecondRapportBlock.setBackgroundColor(Color.parseColor("#ffffff"));
+//        }
 
         return convertView;
     }
